cf8f3602d7da73bf0d8bba7f5dc95c82cc3d4195
Jaydeep <jaydeep.w@gmail.com>
2014-03-16 19:46:40 +0530

    add lable view in the root UI

--- ToDoList/ToDoList/T4DAppDelegate.m
+++ ToDoList/ToDoList/T4DAppDelegate.m
@@ -12,10 +12,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor cyanColor];
-    [self.window makeKeyAndVisible];
     return YES;
 }
 
