9ab4dc5b6dd0e066cb2e253220c9885d3a62ec62
Jaydeep <jaydeep.w@gmail.com>
2014-03-16 18:56:16 +0530

    start to-do app

---
+++ ToDoList/ToDoList/T4DAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  T4DAppDelegate.h
+//  ToDoList
+//
+//  Created by Jaydeep on 16/03/14.
+//  Copyright (c) 2014 Jaydeep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface T4DAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Jaydeep on 16/03/14.
+//  Copyright (c) 2014 Jaydeep. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "T4DAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([T4DAppDelegate class]));
+    }
+}
